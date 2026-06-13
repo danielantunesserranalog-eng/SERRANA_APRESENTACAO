1d6b4bd5bfe5df13b7a318cc17d9c7cab4eafd02
danielantunesserranalog-eng <daniel.antunesserranalog@gmail.com>
2026-06-12 22:16:50 -0300

    atualizei

--- js/config_db.js
+++ js/config_db.js
@@ -9,6 +9,6 @@ const SUPABASE_CONFIG = {
 
 // 2. NOVO Banco de Dados (Operacional e Manutenção)
 const SUPABASE_CONFIG_FROTA = {
-    url: 'https://tjjrzinpogjrquoosuqn.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqanJ6aW5wb2dqcnF1b29zdXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMzMxODksImV4cCI6MjA5NDcwOTE4OX0.IdZOXfXiWeFIUI4LPDVb1sZNyKogo4fOs-_9UcP_xj0'
+    url: 'https://bydlwhosxtmzfqlnyhcz.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5ZGx3aG9zeHRtemZxbG55aGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyOTU1MzQsImV4cCI6MjA5Njg3MTUzNH0.UIQPOUSNLAef6QPGZk6bkRPWvH2DtNu6HoaMv9X4e3Q'
 };
